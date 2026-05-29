9508a4f026292b8a80da45b580eef42e0c045114
fvlm-hub <fernandovalentin6556@gmail.com>
2026-05-29 03:10:20 -0500

    Se mejora formulario de contacto

--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './contacto.html',
-  styleUrl: './contacto.css',
+  styleUrl: './contacto.css'
 })
-export class Contacto {}
+export class Contacto {
+  nombre = '';
+  correo = '';
+  mensaje = '';
+  enviado = false;
+  error = '';
+
+  enviarMensaje() {
+    if (!this.nombre || !this.correo || !this.mensaje) {
+      this.error = 'Completa todos los campos antes de enviar.';
+      this.enviado = false;
+      return;
+    }
+
+    this.error = '';
+    this.enviado = true;
+
+    this.nombre = '';
+    this.correo = '';
+    this.mensaje = '';
+  }
+}
